d98dfbc6050b4198ad0b72b2e9ecb0aed47eb31a
Joaquin Fox <joaquin.fox@gmail.com>
2019-02-12 13:21:26 -0700

    merge with Barret

--- src/components/user-dashboard.js
+++ src/components/user-dashboard.js
@@ -1,22 +1,24 @@
 import React, { Component } from 'react';
-import '../stylesheets/user-dashboard.css'
+import '../stylesheets/user-dashboard.css';
 
-
-export default class UserDashboard extends Component{
-
+export default class UserDashboard extends Component {
   render() {
     return (
       <div className="orgDash">
         <h2>User Name</h2>
 
-        <img className="profilePic" alt="user profile pic" src="https://via.placeholder.com/150" />
+        <img
+          className="profilePic"
+          alt="user profile pic"
+          src="https://via.placeholder.com/150"
+        />
 
         <section className="upcomingEvents">
           <h3 className="eventsHeader">My Upcoming Events</h3>
 
           <div className="eventsContainer">
-          event cards go here....
-          {/* render event card components */}
+            event cards go here....
+            {/* render event card components */}
           </div>
         </section>
 
@@ -24,7 +26,7 @@ export default class UserDashboard extends Component{
           <h3 className="eventsHeader">Events Nearby</h3>
 
           <div className="eventsContainer">
-          event cards go here....
+            event cards go here....
             {/* render event card components */}
           </div>
         </section>
@@ -35,9 +37,7 @@ export default class UserDashboard extends Component{
           <button>Followed Organizations</button>
           <button>Created Organizations</button>
         </article>
-    
       </div>
-      )
-  }  
-  
+    );
+  }
 }
