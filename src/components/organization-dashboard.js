ae5288c46093a77201ee959191d9c9745cd0a7e3
Joaquin Fox <joaquin.fox@gmail.com>
2019-02-13 10:24:48 -0700

    new event form rendering onclick from org dashboard

--- src/components/organization-dashboard.js
+++ src/components/organization-dashboard.js
@@ -1,17 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
+import EventList from './event-list';
 import EventCard from './eventcard';
-import OrgDashboardEventButtons from './orgdashboardeventbuttons';
+import DashboardCreateEvent from './dashboard-create-event'
+// import createOrgForm from './creat-org-form';
 import '../stylesheets/dashboard.css';
 
 const OrganizationDashboard = () => {
+  const [showView, setView] = useState(EventList);
   return (
-    <div className="main-dashboard">
-      <div className="org-blob">ORG BLOB</div>
-      <div className="dashboard-content">
-        <OrgDashboardEventButtons />
-        <EventCard />
-      </div>
-    </div>
+    <article className="orgDash">
+        <h2>ORG NAME</h2>
+
+        <img className="profilePic" alt="user profile pic" src="https://via.placeholder.com/150" />
+
+        {showView}
+
+        <article className="nav">
+        <button onClick={()=>setView(DashboardCreateEvent)}>Create a new event</button>
+          {/* <button onClick={() => setView(EventList)}>My Events</button> */}
+          {/* <button onClick={setView()}>Find Events</button> */}
+          {/* <button onClick={() => setView(createOrgForm)}>Create Organization</button> */}
+          {/* <button onClick={() => setView(FollowedOrgs)}>Followed Organizations</button> */}
+          {/* <button onClick={() =>setView(CreatedOrgs)}>Created Organizations</button> */}
+        </article>
+    
+      </article>
+    // <div className="main-dashboard">
+    //   <div className="org-blob">ORG BLOB</div>
+    //   <div className="dashboard-content">
+    //     {/* <OrgDashboardEventButtons /> */}
+    //     <EventCard />
+    //   </div>
+    // </div>
   );
 };
 
