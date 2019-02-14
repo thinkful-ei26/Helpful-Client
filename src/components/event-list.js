dac811e422b3449e574ebc5afaacb106876c7189
carpenter-js <carpenterbarrett@gmail.com>
2019-02-14 07:38:11 -0700

    added some axios

--- src/components/event-list.js
+++ src/components/event-list.js
@@ -1,12 +1,28 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
 import EventCard from './eventcard'
+import {API_BASE_URL} from '../config';
+
 import '../stylesheets/event-list.css';
 
 export default function EventList(props) {
+  // let events = [];
+
+  const [events, setEvents] = useState(null);
+
+  const fetchData = async() => {
+    const eventsRequest = await axios(
+        `${API_BASE_URL}/event/all`,
+    );
+    console.log('setEvents setting to: ', eventsRequest.data)
+    return setEvents(eventsRequest.data);
+  }
 
   useEffect(() => {
-    
-  })
+    fetchData();
+  }, []);
+  
+  if (events) {
 
   return (
     <article>
@@ -14,7 +30,7 @@ export default function EventList(props) {
           <h3 className="eventsHeader">My Upcoming Events</h3>
 
           <div className="eventsContainer">
-            <EventCard />
+            <EventCard event={events[0]}/>
             <EventCard />
             <EventCard />
           </div>
@@ -23,12 +39,17 @@ export default function EventList(props) {
     <section className="nearbyEvents">
       <h3 className="eventsHeader">Events Nearby</h3>
 
-      <div className="eventsContainer">
+      {/* <div className="eventsContainer">
         <EventCard />
         <EventCard />
         <EventCard />
-      </div>
+      </div> */}
     </section>
     </article>
   )
+  }
+  return (
+    <p>loading...</p>
+  )
+  
 }
