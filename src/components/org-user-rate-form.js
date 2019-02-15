2148e7720d41e2765de6c31e09261a3591eb78b9
Joaquin Fox <joaquin.fox@gmail.com>
2019-02-15 13:31:48 -0700

    user can rate org, static component setup

---
+++ src/components/org-user-rate-form.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const OrgUserCanRate = () => {
+  return (
+    <div>
+      <form action="user-rating">
+        {/* <fieldset> */}
+          {/* <legend>Rate us!</legend> */}
+        {/* </fieldset> */}
+        <select name="" id="">
+          <option value="">1</option>
+          <option value="">2</option>
+          <option value="">3</option>
+          <option value="">4</option>
+          <option value="">5</option>
+        </select>
+      </form>
+    </div>
+  );
+};
+
+export default OrgUserCanRate;
