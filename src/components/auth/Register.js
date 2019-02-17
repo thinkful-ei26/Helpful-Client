3198b74d239945da037f726d2e694d3d0b44ae09
Fantosism <businessspamfolder@gmail.com>
2019-02-17 15:48:22 -0700

    working on css for organization public page, refactoring for materialize framework

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -1,101 +1,94 @@
-import React, { useState, useEffect } from "react";
-import axios from 'axios';
-import { Link, Route } from "react-router-dom";
-import {API_BASE_URL} from '../../config';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { registerUser } from "../../actions/authActions";
 
 const Register = () => {
-
+  // onChange = e => {
+  //   this.setState({ [e.target.id]: e.target.value })
+  // }
+  // Non-asynchronous actions are very similar to useState hook
+  // window.localStorage.getitem(user) || localStorage
   const [userName, setUserName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [passwordConfirmation, setPasswordConfirmation] = useState("");
-
-  const createUser = async(history) => {
-    let data = {
-      username: userName,
-      email,
-      password,
-    }
-    await axios({
-      method: 'post',
-      url: `${API_BASE_URL}/users/register`,
-      data,
-    });
-    history.push(`/login`)
-  }
-
+  // const [submitting, setSubmitting] = useState(false);
+  // const newUser = {
+  //   userName: state.userName,
+  //   email: state.email,
+  //   password: state.password,
+  //   passwordConfirmation: state.passwordConfirmation
+  // };
+  // registerUser(newUser, this.props.history);
   return (
-    <Route render={({history}) => (
-      <div className="container">
-        <div className="row">
-          <div className="col s8 offset-s2">
-            <Link to="/" className=" waves-effect btn-flat">
-              <i className="material-icons left">keyboard_backspace</i>
-              Back to home
-            </Link>
-            <div className="col s12" style={{ paddingLeft: "11.25px" }}>
-              <h4>Register below</h4>
-              <p className="grey-text text-darken-1">
-                Already have an account? <Link to="/login">Log In</Link>
-              </p>
-            </div>
-            <form noValidate onSubmit={e => e.preventDefault()}>
-              <div className="input-field col s12">
-                <input
-                  value={userName}
-                  onChange={e => setUserName(e.target.value)}
-                  id="name"
-                  type="text"
-                />
-                <label htmlFor="name">Name</label>
-              </div>
-              <div className="input-field col s12">
-                <input
-                  value={email}
-                  onChange={e => setEmail(e.target.value)}
-                  id="email"
-                  type="email"
-                />
-                <label htmlFor="email">Email</label>
-              </div>
-              <div className="input-field col s12">
-                <input
-                  value={password}
-                  onChange={e => setPassword(e.target.value)}
-                  id="password"
-                  type="password"
-                />
-                <label htmlFor="password">Password</label>
-              </div>
-              <div className="input-field col s12">
-                <input
-                  value={passwordConfirmation}
-                  onChange={e => setPasswordConfirmation(e.target.value)}
-                  id="passwordConfirmation"
-                  type="password"
-                />
-                <label htmlFor="passwordConfirmation">Confirm Password</label>
-              </div>
-              <div className="col s12" style={{ paddingLeft: "11.25px" }}>
-                <button
-                  onClick={() => createUser(history)}
-                  style={{
-                    width: "150px",
-                    borderRadius: "3px",
-                    letterSpacing: "1.5px",
-                    marginTop: "1rem"
-                  }}
-                  type="submit"
-                  className="btn btn-large waves-effect waves-light hoverable teal lighten-2"
-                >
-                  Sign Up
-                </button>
-              </div>
-            </form>
+    <div className="container">
+      <div className="row" style={{ marginTop: "4rem" }}>
+        <div className="col s8 offset-s2">
+          <Link to="/" className=" waves-effect btn-flat">
+            <i className="material-icons left">keyboard_backspace</i>
+            Back to home
+          </Link>
+          <div className="col s12" style={{ paddingLeft: "11.25px" }}>
+            <h4>Register below</h4>
+            <p className="grey-text text-darken-1">
+              Already have an account? <Link to="/login">Log In</Link>
+            </p>
           </div>
+          <form noValidate onSubmit={e => e.preventDefault()}>
+            <div className="input-field col s12">
+              <input
+                value={userName}
+                onChange={e => setUserName(e.target.value)}
+                id="name"
+                type="text"
+              />
+              <label htmlFor="name">Name</label>
+            </div>
+            <div className="input-field col s12">
+              <input
+                value={email}
+                onChange={e => setEmail(e.target.value)}
+                id="email"
+                type="email"
+              />
+              <label htmlFor="email">Email</label>
+            </div>
+            <div className="input-field col s12">
+              <input
+                value={password}
+                onChange={e => setPassword(e.target.value)}
+                id="password"
+                type="password"
+              />
+              <label htmlFor="password">Password</label>
+            </div>
+            <div className="input-field col s12">
+              <input
+                value={passwordConfirmation}
+                onChange={e => setPasswordConfirmation(e.target.value)}
+                id="passwordConfirmation"
+                type="password"
+              />
+              <label htmlFor="passwordConfirmation">Confirm Password</label>
+            </div>
+            <div className="col s12" style={{ paddingLeft: "11.25px" }}>
+              <button
+                style={{
+                  width: "150px",
+                  borderRadius: "3px",
+                  letterSpacing: "1.5px",
+                  marginTop: "1rem"
+                }}
+                type="submit"
+                className="btn btn-large waves-effect waves-light hoverable teal lighten-2"
+              >
+                Sign Up
+              </button>
+            </div>
+          </form>
         </div>
       </div>
-    )} />
+    </div>
   );
 };
 
--- src/components/org-public-page.js
+++ src/components/org-public-page.js
@@ -1,30 +1,56 @@
-import React, { useState } from 'react';
-import OrgPublicPageEventList from './org-public-page-event-list';
-import '../stylesheets/org-public-page.css';
+import React, { useState } from "react";
+import OrgPublicPageEventList from "./org-public-page-event-list";
+import "../stylesheets/org-public-page.css";
+import M from "materialize-css";
 
 export default function OrgPublicPage() {
   const [view] = useState(<OrgPublicPageEventList />);
+  document.addEventListener("DOMContentLoaded", function() {
+    let elems = document.querySelectorAll(".fixed-action-btn");
+    let instances = M.FloatingActionButton.init(elems, {
+      direction: "left",
+      // toolbarEnabled: true
+      hoverEnabled: true
+    });
+  });
   return (
-    <div className="org-public-page-main">
-      <div className="org-public-header">
-        <div className="org-public-logo"></div>
-        <div className="org-public-social">
-          <ul>
-            <li>link 1</li>
-            <li>link 2</li>
-            <li>link 3</li>
-          </ul>
-        </div>
+    <div className="org-public-page-main center container valign-wrapper">
+      <div class="fixed-action-btn">
+        <a class="btn-floating btn-large teal lighten-2">
+          <i class="large material-icons">add</i>
+        </a>
+        <ul>
+          <li>
+            <a class="btn-floating red">
+              <i class="material-icons">comment</i>
+            </a>
+          </li>
+          <li>
+            <a class="btn-floating pink darken-4">
+              <i class="material-icons">share</i>
+            </a>
+          </li>
+          <li>
+            <a class="btn-floating purple darken-2">
+              <i class="material-icons">publish</i>
+            </a>
+          </li>
+          <li>
+            <a class="btn-floating light-blue darken-3">
+              <i class="material-icons">notifications</i>
+            </a>
+          </li>
+        </ul>
       </div>
 
-      <div className="org-public-content">
-        <div className="org-public-calltoaction">
-          <button>Follow us</button>
-          <button disabled>Button 2</button>
-          <button disabled>Button 3</button>
-        </div>
+      <div className="org-public-content container">
         <div className="org-public-text-area">
-          <h1>Organization name</h1>
+          <h1>Organization Name</h1>
+          <img
+            alt="Organization Logo"
+            className="responsive-img"
+            src="http://lorempixel.com/200/200/"
+          />
           <p>
             Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
             officia deserunt mollit anim id est laborum.
@@ -37,10 +63,8 @@ export default function OrgPublicPage() {
           </p>
           <p>lorem ipsum</p>
         </div>
+        <div className="org-public-events">{view}</div>
       </div>
-      <div className="org-public-events">{view}</div>
-
-      <div className="org-public-footer">Footer</div>
     </div>
   );
 }
