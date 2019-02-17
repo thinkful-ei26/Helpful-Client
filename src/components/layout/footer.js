ed8e9402dcf2e140fd3a153cd383563256605f3c
Fantosism <businessspamfolder@gmail.com>
2019-02-17 15:49:00 -0700

    created footer component

---
+++ src/components/layout/footer.js
@@ -0,0 +1,51 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer class="page-footer teal">
+      <div class="container">
+        <div class="row">
+          <div class="col l6 s12">
+            <h5 class="white-text">About Helpful</h5>
+            <p class="grey-text text-lighten-4">
+              Cronut kickstarter brooklyn edison bulb lo-fi. DIY intelligentsia
+              butcher, glossier fam shoreditch pour-over listicle shabby chic
+              bitters authentic bicycle rights VHS. Art party leggings quinoa
+              organic.
+            </p>
+          </div>
+          <div class="col l3 s12">
+            <h5 class="white-text">Settings</h5>
+            <ul>
+              <li>
+                <a class="white-text" href="#!">
+                  Link 1
+                </a>
+              </li>
+              <li>
+                <a class="white-text" href="#!">
+                  Link 2
+                </a>
+              </li>
+              <li>
+                <a class="white-text" href="#!">
+                  Link 3
+                </a>
+              </li>
+              <li>
+                <a class="white-text" href="#!">
+                  Link 4
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="footer-copyright">
+        <div class="container">Marfa hot chicken shoreditch thundercats</div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
