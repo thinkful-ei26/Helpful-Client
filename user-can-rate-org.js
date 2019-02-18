89b5abd32fe0514222292c416215b9c6dc92acf4
Joaquin Fox <joaquin.fox@gmail.com>
2019-02-18 12:57:58 -0700

    user can rate begin

---
+++ user-can-rate-org.js
@@ -0,0 +1,58 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { API_BASE_URL } from '../config';
+import M from "materialize-css";
+
+export default function UserCanRateOrg() {
+  const [formData, setFormData] = useState({
+    rating: ''
+  });
+
+  const [success, setSuccess] = useState(false);
+
+  const postRating = async () => {
+    const postRatingResult = await axios({
+      method: 'post',
+      url: `${API_BASE_URL}/org`,
+      data: formData
+    });
+    setSuccess(true);
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    postRating();
+  };
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('select');
+    var instances = M.FormSelect.init(elems, {});
+  });
+
+  if (success) {
+    return <p className="orgCreateSuccess">Thank you for rating us!</p>;
+  }
+  return (
+    <form
+      action="submit"
+      className="create-org-form"
+      id="createOrgForm"
+      onSubmit={e => handleSubmit(e)}
+    >
+      <fieldset>
+        <legend>Create an Organization</legend>
+
+        <div className="create-org-row">
+          <label htmlFor="org-name"> Organization name</label>
+          <input
+            type="text"
+            placeholder="Organization name"
+            onChange={e => setFormData({ ...formData, name: e.target.value })}
+          />
+        </div>
+
+        <button type="submit">Submit</button>
+      </fieldset>
+    </form>
+  );
+}
