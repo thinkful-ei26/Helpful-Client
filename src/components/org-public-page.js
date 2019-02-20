94b4f63a8ace5104a2036d0d1a25c1f510dd76e7
Fantosism <businessspamfolder@gmail.com>
2019-02-20 13:43:10 -0700

    dynamic data for organization pages

--- src/components/org-public-page.js
+++ src/components/org-public-page.js
@@ -3,74 +3,74 @@ import OrgPublicPageEventList from "./org-public-page-event-list";
 import UserCanRateOrg from "./user-can-rate-org";
 import "../stylesheets/org-public-page.css";
 import M from "materialize-css";
+import axios from "axios";
+import { API_BASE_URL } from "../config";
+import getOrgs from "../utils/fetchOrg";
 
-export default function OrgPublicPage() {
-  const [view] = useState(<OrgPublicPageEventList />);
+const OrgPublicPage = function(props) {
+  const [view, setView] = useState(<OrgPublicPageEventList />);
+  const [orgs, setOrgs] = useState(null);
 
-  useEffect(() => {
-    let elems = document.querySelectorAll(".fixed-action-btn");
-    let instances = M.FloatingActionButton.init(elems, {
-      direction: "left",
-      // toolbarEnabled: true
-      hoverEnabled: true
-    });
-    return instances;
-  }, []);
+  const fetchData = props => {
+    getOrgs(props.match.params.id).then(res => setOrgs(res.data));
+  };
 
-  // document.addEventListener('DOMContentLoaded', function() {
-  //   let elems = document.querySelectorAll('.fixed-action-btn');
-  //   let instances = M.FloatingActionButton.init(elems, {
-  //     direction: 'left',
-  //     // toolbarEnabled: true
-  //     hoverEnabled: true
-  //   });
-  // });
-  return (
-    <div className="org-public-page-main center container valign-wrapper">
-      <div className="fixed-action-btn">
-        <a className="btn-floating btn-large teal lighten-2">
-          <i className="large material-icons">add</i>
-        </a>
-        <ul>
-          <li>
-            <a className="btn-floating red">
-              <i className="material-icons">comment</i>
-            </a>
-          </li>
-          <li>
-            <a className="btn-floating pink darken-4">
-              <i className="material-icons">share</i>
-            </a>
-          </li>
-          <li>
-            <a className="btn-floating purple darken-2">
-              <i className="material-icons">publish</i>
-            </a>
-          </li>
-          <li>
-            <a className="btn-floating light-blue darken-3">
-              <i className="material-icons">notifications</i>
-            </a>
-          </li>
-        </ul>
-      </div>
+  useEffect(() => fetchData(props), [props.match.params.id]);
 
-      <div className="org-public-content container">
-        <div className="org-public-text-area">
-          <h1>Organization Name</h1>
-          <img alt="Organization Logo" className="responsive-img" src="http://lorempixel.com/200/200/" />
-          <UserCanRateOrg />
-          <p className="flow-text">
-            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
-            laborum.
-          </p>
-          <p className="flow-text">
-            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-          </p>
+  console.log("LOOK AT ME", props.match.params.id);
+
+  if (orgs) {
+    return (
+      <div className="org-public-page-main center container valign-wrapper">
+        <div className="fixed-action-btn">
+          <a className="btn-floating btn-large teal lighten-2">
+            <i className="large material-icons">add</i>
+          </a>
+          <ul>
+            <li>
+              <a className="btn-floating red">
+                <i className="material-icons">comment</i>
+              </a>
+            </li>
+            <li>
+              <a className="btn-floating pink darken-4">
+                <i className="material-icons">share</i>
+              </a>
+            </li>
+            <li>
+              <a className="btn-floating purple darken-2">
+                <i className="material-icons">publish</i>
+              </a>
+            </li>
+            <li>
+              <a className="btn-floating light-blue darken-3">
+                <i className="material-icons">notifications</i>
+              </a>
+            </li>
+          </ul>
         </div>
-        <div className="org-public-events">{view}</div>
+
+        <div className="org-public-content container">
+          <div className="org-public-text-area">
+            <span className="header">{orgs.name}</span>
+            <img
+              alt="Organization Logo"
+              className="responsive-img"
+              src={orgs.imgUrl}
+            />
+            {/* <UserCanRateOrg /> */}
+            <p className="flow-text">{orgs.description}</p>
+            <p className="flow-text">{orgs.location}</p>
+          </div>
+        </div>
       </div>
+    );
+  }
+  return (
+    <div>
+      <p>I am loading text</p>
     </div>
   );
-}
+};
+
+export default OrgPublicPage;
--- src/components/search-org.js
+++ src/components/search-org.js
@@ -1,60 +1,69 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import { API_BASE_URL } from '../config';
-import { InitialMap } from './map';
-import '../stylesheets/search-org.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import { API_BASE_URL } from "../config";
+import { InitialMap } from "./map";
+import "../stylesheets/search-org.css";
 
 export default function SearchOrg(props) {
+  // const [orgs, setOrgs] = useState(null);
 
-    // const [orgs, setOrgs] = useState(null);
+  // const fetchData = async () => {
 
-    // const fetchData = async () => {
+  //     const getOrgs = await axios({
+  //         method: 'get',
+  //         url: `${API_BASE_URL}/org/all`,
+  //         data: {
+  //         },
+  //         headers: {
+  //             'Content-Type': 'application/json',
+  //             'Authorization': 'Bearer '.concat(localStorage.getItem("jwtToken"))
+  //         }
+  //     });
+  //     setOrgs(getOrgs.data)
+  // }
 
-    //     const getOrgs = await axios({
-    //         method: 'get',
-    //         url: `${API_BASE_URL}/org/all`,
-    //         data: {
-    //         },
-    //         headers: {
-    //             'Content-Type': 'application/json',
-    //             'Authorization': 'Bearer '.concat(localStorage.getItem("jwtToken"))
-    //         }
-    //     });
-    //     setOrgs(getOrgs.data)
-    // }
+  // useEffect(() => {
+  //     fetchData();
+  // }, []);
+  //   onClick={() => props.history.push("/organization")}
 
-    // useEffect(() => {
-    //     fetchData();
-    // }, []);
-
-    if (props.orgs) {
-        const listNames = props.orgs.map(org => {
-            return <div onClick={() => props.history.push('/organization')} class="card blue-grey darken-1">
-                <div class="card-content white-text">
-                    <span class="card-title">{org.name}</span>
-                    <p>{org.description}</p>
-                </div>
-                <div class="card-action">
-                    <span>{org.date}</span>
-                    <span>{org.location}</span>
-                    <span>{org.contact}</span>
-                </div>
+  if (props.orgs) {
+    const listNames = props.orgs.map(org => {
+      return (
+        <Link to={`/organization/${org.id}`}>
+          <div class="card blue-grey darken-1">
+            <div class="card-content white-text">
+              <span class="card-title">{org.name}</span>
+              <p>{org.description}</p>
             </div>
-        })
+            <div class="card-action">
+              <span>{org.date}</span>
+              <span>{org.location}</span>
+              <span>{org.contact}</span>
+            </div>
+          </div>
+        </Link>
+      );
+    });
 
-        return (<div>
-            <InitialMap
-                isMarkerShown
-                googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
-                loadingElement={<div style={{ height: `100%` }} />}
-                containerElement={<div style={{ height: `400px` }} />}
-                mapElement={<div style={{ height: `100%` }} />}
-            />
-            <h3>{listNames}</h3>
-        </div>)
-    } else {
-        return (<div>
-            <h2> Loading organizations...</h2>
-        </div>)
-    }
+    return (
+      <div>
+        <InitialMap
+          isMarkerShown
+          googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
+          loadingElement={<div style={{ height: `100%` }} />}
+          containerElement={<div style={{ height: `400px` }} />}
+          mapElement={<div style={{ height: `100%` }} />}
+        />
+        <h3>{listNames}</h3>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <h2> Loading organizations...</h2>
+      </div>
+    );
+  }
 }
