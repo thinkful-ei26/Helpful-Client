e704efd9c5047c2351ecdc3a41a27ddf0b4be5a2
Joaquin Fox <joaquin.fox@gmail.com>
2019-02-21 10:23:40 -0700

    comments posted and peristed

--- src/components/usercomments.js
+++ src/components/usercomments.js
@@ -1,17 +1,74 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { API_BASE_URL } from '../config';
+import axios from 'axios';
 
 const UserComments = () => {
   const [comment, setComment] = useState('');
   const [comments, setComments] = useState([]);
   const onChange = event => {
     setComment(event.target.value);
+    console.log(event.target.value);
   };
-  
+
+  const createComment = async comment => {
+    const createCommentResult = await axios({
+      method: 'post',
+      url: `${API_BASE_URL}/comments`,
+      data: { comment, orgId: '5c6db84cca88852e5c895aeb' },
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer '.concat(localStorage.getItem('jwtToken'))
+      }
+    })
+      .then(() => {
+        return '';
+      })
+      .then(() => {
+        fetchComments();
+      })
+      .catch(error => console.log(error));
+  };
+
+  // const fetchComments = async comment => {
+  //   const fetchCommentsResult = await axios({
+  //     method: 'get',
+  //     url: `${API_BASE_URL}/comments`,
+  //     // data: { comment, orgId: '5c6db84cca88852e5c895aeb' },
+  //     headers: {
+  //       'Content-Type': 'application/json',
+  //       Authorization: 'Bearer '.concat(localStorage.getItem('jwtToken'))
+  //     }
+  //   });
+  //   setComments(fetchCommentsResult.data)
+  // };
+  /******************************************* */
+  const fetchComments = async () => {
+    const fetchCommentsResult = await axios(`${API_BASE_URL}/comments`, {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer '.concat(localStorage.getItem('jwtToken'))
+      }
+    });
+    setComments(fetchCommentsResult.data);
+  };
+  /****************************************************************************/
+  useEffect(() => {
+    fetchComments();
+  }, []);
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    createComment();
+  };
+
   const onSubmit = event => {
+    console.log('************', comment);
     event.preventDefault();
-    setComment('');
     setComments([...comments, comment]);
+    createComment(comment);
   };
+
+  const mapComments = comments.map((item, index) => <li key={index}>{item.comment}</li>);
 
   return (
     <div>
@@ -20,11 +77,7 @@ const UserComments = () => {
         <textarea style={{ background: '#ccc' }} onChange={onChange} />
         <button type="submit">Add a public comment</button>
       </form>
-      <ul>
-        {comments.map(item => (
-          <li key={item}>{item}</li>
-        ))}
-      </ul>
+      <ul>{mapComments}</ul>
     </div>
   );
 };
