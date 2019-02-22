5bf412d4dca688e603f92ff4faa7c314a5aa1ead
Joaquin Fox <joaquin.fox@gmail.com>
2019-02-22 14:35:58 -0700

    updates to user ratings

--- src/components/user-can-rate-org.js
+++ src/components/user-can-rate-org.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React, { useState } from "react";
 import axios from "axios";
 import { API_BASE_URL } from "../config";
@@ -10,6 +11,17 @@ export default function UserCanRateOrg() {
     const [success, setSuccess] = useState(false);
 
 <<<<<<< HEAD
+=======
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { API_BASE_URL } from '../config';
+import M from 'materialize-css';
+
+export default function UserCanRateOrg() {
+  const [formData, setFormData] = useState({
+    rating: ''
+  });
+>>>>>>> updates to rating
 
     const postRating = async () => {
         await axios({
@@ -67,7 +79,7 @@ export default function UserCanRateOrg() {
 =======
   const postRating = async () => {
     const postRatingResult = await axios({
-      method: "post",
+      method: 'post',
       url: `${API_BASE_URL}/org`,
       data: formData
     });
@@ -79,41 +91,39 @@ export default function UserCanRateOrg() {
     postRating();
   };
 
-  document.addEventListener("DOMContentLoaded", function() {
-    var elems = document.querySelectorAll("select");
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('select');
     var instances = M.FormSelect.init(elems, {});
   });
- 
+
   if (success) {
     return <p className="orgCreateSuccess">Thank you for rating us!</p>;
   }
   //5c6ad21d8368687005177507 orgId for test post
   return (
-    // <div className="container">
-    //   <div className="row">
-    //     <div className="center">
-    //       <form action="submit" className="" onSubmit={e => handleSubmit(e)}>
-            // <fieldset>
-              // <div className="create-org-row">
-                <div style={{heigh:'500px',color:'red'}}>
-                  <select className='browser-default'>
-                    <option value="" disabled selected>
-                      Rate this group
-                    </option>
-                    <option value="1">1 star</option>
-                    <option value="2">2 stars</option>
-                    <option value="3">3 stars</option>
-                    <option value="4">4 stars</option>
-                    <option value="5">5 stars</option>
+    <div className="container">
+      <div className="row">
+        <div className="center">
+          <form action="submit" className="" onSubmit={e => handleSubmit(e)}>
+            <fieldset>
+              <div className="create-org-row">
+                <div style={{ heigh: '500px', color: 'red' }}>
+                  <label>Rate this group</label>
+                  <select className="browser-default">
+                    <option defaultValue="1">1 star</option>
+                    <option defaultValue="2">2 stars</option>
+                    <option defaultValue="3">3 stars</option>
+                    <option defaultValue="4">4 stars</option>
+                    <option defaultValue="5">5 stars</option>
                   </select>
                 </div>
-              // </div>
-              // <a className="waves-effect waves-light btn">Submit</a>
-            // </fieldset>
-    //       </form>
-    //     </div>
-    //   </div>
-    // </div>
+              </div>
+              <a className="waves-effect waves-light btn">Submit</a>
+            </fieldset>
+          </form>
+        </div>
+      </div>
+    </div>
   );
 >>>>>>> updates
 }
