7e9d94799c49d2e8e9614d06ba757a9969343b40
Fantosism <businessspamfolder@gmail.com>
2019-02-22 13:50:30 -0700

    deleting actions and reducers for old redux setup

--- src/actions/setAuthToken.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-const setAuthToken = token => {
-    token
-        ? (axios.defaults.headers.common["Authorization"] = token)
-        : delete axios.defaults.headers.common["Authorization"];
-};
-
-export default setAuthToken;
