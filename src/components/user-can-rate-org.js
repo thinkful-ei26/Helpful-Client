661212b90e1f5c6abe28000d0f82d4038155b965
Joaquin Fox <joaquin.fox@gmail.com>
2019-02-24 12:13:56 -0700

    refactor select to input list with parameters and number type

--- src/components/user-can-rate-org.js
+++ src/components/user-can-rate-org.js
@@ -1,91 +1,110 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import { API_BASE_URL } from '../config';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { API_BASE_URL } from "../config";
 
 export default function UserCanRateOrg() {
-  const [rating, setRating] = useState(0);
-  const [ratings, setRatings] = useState([]);
+    const [rating, setRating] = useState(0);
+    const [ratings, setRatings] = useState([]);
 
-  const onChange = event => {
-    setRating(event.target.value);
-  };
+    const onChange = event => {
+        setRating(event.target.value);
+    };
 
-  const createRating = async rating => {
-    await axios({
-      method: 'post',
-      url: `${API_BASE_URL}/orgrating`,
-      data: { rating },
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: 'Bearer '.concat(localStorage.getItem('jwtToken'))
-      }
-    })
-      .then(() => {
-        return rating;
-      })
-      .then(() => {
+    const createRating = async rating => {
+        await axios({
+            method: "post",
+            url: `${API_BASE_URL}/orgrating`,
+            data: { rating },
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: "Bearer ".concat(
+                    localStorage.getItem("jwtToken")
+                ),
+            },
+        })
+            .then(() => {
+                return rating;
+            })
+            .then(() => {
+                console.log("$$$$$$$$$$$$$$", rating);
+                fetchRatings();
+            })
+            .catch(error => {
+                console.log(error);
+                console.log(
+                    "Your POST request did not complete, most likely because you have allready rated this group and ratings need to be unique"
+                );
+            });
+    };
+    const fetchRatings = async () => {
+        const fetchRatingsResult = await axios(`${API_BASE_URL}/orgrating`, {
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: "Bearer ".concat(
+                    localStorage.getItem("jwtToken")
+                ),
+            },
+        });
+
+        setRatings(fetchRatingsResult.data);
+    };
+
+    useEffect(() => {
         fetchRatings();
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  };
-  const fetchRatings = async () => {
-    const fetchRatingsResult = await axios(`${API_BASE_URL}/orgrating`, {
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: 'Bearer '.concat(localStorage.getItem('jwtToken'))
-      }
-    });
+    }, []);
 
-    setRatings(fetchRatingsResult.data);
-  };
+    const onSubmit = event => {
+        event.preventDefault();
+        setRatings([...ratings, rating]);
+        createRating(rating);
+    };
 
-  useEffect(() => {
-    fetchRatings();
-  }, []);
+    const ratingAvg = ratings => {
+        let result = [];
+        ratings.map(obj => {
+            result.push(obj.rating);
+        });
+        return result.reduce((a, b) => a + b, 0) / result.length;
+    };
 
-  const onSubmit = event => {
-    event.preventDefault();
-    setRatings([...ratings, rating]);
-    createRating(rating);
-  };
+    return (
+        <div className='container'>
+            <div className='row'>
+                <div className='center'>
+                    <form onSubmit={onSubmit}>
+                        <fieldset>
+                            <div className='create-org-row'>
+                                <div style={{ heigh: "500px", color: "red" }}>
+                                    <label>Rate this group</label>
 
-  const ratingAvg = ratings => {
-    let result = [];
-    ratings.map(obj => {
-      result.push(obj.rating);
-    });
-    return result.reduce((a, b) => a + b, 0) / result.length;
-  };
-
-  return (
-    <div className="container">
-      <div className="row">
-        <div className="center">
-          <form onSubmit={onSubmit}>
-            <fieldset>
-              <div className="create-org-row">
-                <div style={{ heigh: '500px', color: 'red' }}>
-                  <label>Rate this group</label>
-                  <select onChange={onChange} className="browser-default">
-                    <option defaultValue="1">1</option>
-                    <option defaultValue="2">2</option>
-                    <option defaultValue="3">3 </option>
-                    <option defaultValue="4">4</option>
-                    <option defaultValue="5">5 </option>
-                  </select>
+                                    <input
+                                        type='number'
+                                        list='userrating'
+                                        min='1'
+                                        max='5'
+                                    />
+                                    <ratinglist
+                                        id='rating'
+                                        onChange={onChange}
+                                        className='browser-default'>
+                                        <option defaultValue='1' />
+                                        <option defaultValue='2' />
+                                        <option defaultValue='3' />
+                                        <option defaultValue='4' />
+                                        <option defaultValue='5' />
+                                    </ratinglist>
+                                </div>
+                            </div>
+                        </fieldset>
+                        <button type='submit'>Submit your rating</button>
+                    </form>
+                    <div>
+                        Average Rating: {ratingAvg(ratings)} out of{" "}
+                        {ratings.length} reviews
+                    </div>
+                    <ul> Your Rating: {rating}</ul>
                 </div>
-              </div>
-            </fieldset>
-          </form>
-          <div>
-            {' '}
-            Average Rating: {ratingAvg(ratings)} out of {ratings.length} reviews
-          </div>
-          <ul> Your Rating: {rating}</ul>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
