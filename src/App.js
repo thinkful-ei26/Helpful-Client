072c515d8e045a14026e0dcda3cd72a2aca56249
Joaquin Fox <joaquin.fox@gmail.com>
2019-02-25 14:35:53 -0700

    style updates

--- src/App.js
+++ src/App.js
@@ -2,21 +2,21 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 import NavBar from "./components/layout/navbar";
-// import LandingPage from "./components/layout/landing";
-// import Register from "./components/auth/Register";
-// import Login from "./components/auth/Login";
-// import UserDashboard from "./components/user-dashboard";
-// import Search from "./components/search";
-// import OrgPublicPage from "./components/org-public-page";
-// import EventPage from "./components/event-page";
-// import Footer from "./components/layout/footer";
-// import CreateOrgForm from "./components/creat-org-form";
-// import FollowedOrgs from "./components/followed-organizations";
-// import ErrorPage from "./components/layout/errorPage";
-// import CreateMeetup from "./components/create-meetup";
-// import CreatedOrgs from "./components/created-organizations";
-// import OrganizationDashboard from "./components/organization-dashboard";
-// import EventList from "./components/event-list";
+import LandingPage from "./components/layout/landing";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
+import UserDashboard from "./components/user-dashboard";
+import Search from "./components/search";
+import OrgPublicPage from "./components/org-public-page";
+import EventPage from "./components/event-page";
+import Footer from "./components/layout/footer";
+import CreateOrgForm from "./components/creat-org-form";
+import FollowedOrgs from "./components/followed-organizations";
+import ErrorPage from "./components/layout/errorPage";
+import CreateMeetup from "./components/create-meetup";
+import CreatedOrgs from "./components/created-organizations";
+import OrganizationDashboard from "./components/organization-dashboard";
+import EventList from "./components/event-list";
 
 const App = () => {
 
@@ -26,47 +26,47 @@ const App = () => {
                 <NavBar />
                 {/* <OrganizationList /> */}
                 <Switch>
-                    {/* <Route exact path='/' component={LandingPage} /> */}
+                    <Route exact path='/' component={LandingPage} />
                     <Route
                         exact
                         path='/createorgform'
-                        // component={CreateOrgForm}
+                        component={CreateOrgForm}
                     />
                     <Route
                         exact
                         path='/followedorgs'
-                        // component={FollowedOrgs}
+                        component={FollowedOrgs}
                     />
-                    {/* <Route exact path='/register' component={Register} /> */}
-                    {/* <Route exact path='/login' component={Login} /> */}
-                    {/* <Route exact path='/dashboard' component={UserDashboard} /> */}
-                    {/* <Route exact path='/search' component={Search} /> */}
-                    {/* <Route exact path="/organization" component={OrgPublicPage} /> */}
+                    <Route exact path='/register' component={Register} />
+                    <Route exact path='/login' component={Login} />
+                    <Route exact path='/dashboard' component={UserDashboard} />
+                    <Route exact path='/search' component={Search} />
+                    <Route exact path="/organization" component={OrgPublicPage} />
                     <Route
                         exact
                         path='/event/:eventId'
-                        // component={EventPage}
+                        component={EventPage}
                     />
                     <Route
                         exact
                         path='/create-meetup'
-                        // component={CreateMeetup}
+                        component={CreateMeetup}
                     />
                     <Route
                         exact
                         path='/organization/:id'
-                        // component={OrgPublicPage}
+                        component={OrgPublicPage}
                     />
-                    {/* <Route exact path='/createdorgs' component={CreatedOrgs} /> */}
+                    <Route exact path='/createdorgs' component={CreatedOrgs} />
                     <Route
                         exact
                         path='/orgdashboard/:id'
-                        // component={OrganizationDashboard}
+                        component={OrganizationDashboard}
                     />
-                    {/* <Route exact path='/events' component={EventList} /> */}
-                    {/* <Route component={ErrorPage} /> */}
+                    <Route exact path='/events' component={EventList} />
+                    <Route component={ErrorPage} />
                 </Switch>
-                {/* <Footer /> */}
+                <Footer />
             </div>
         </Router>
     );
