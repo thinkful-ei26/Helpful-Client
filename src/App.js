d0c8b7690f92bfefc8cb14af5c462d781cb195f3
Joaquin Fox <joaquin.fox@gmail.com>
2019-02-25 18:35:33 -0700

    updates

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 import NavBar from "./components/layout/navbar";
 import LandingPage from "./components/layout/landing";
-// import Register from "./components/auth/Register";
-// import Login from "./components/auth/Login";
 import UserDashboard from "./components/user-dashboard";
 import Search from "./components/search";
 import OrgPublicPage from "./components/org-public-page";
@@ -37,8 +35,6 @@ const App = () => {
                         path='/followedorgs'
                         component={FollowedOrgs}
                     /> */}
-                    {/* <Route exact path='/register' component={Register} /> */}
-                    {/* <Route exact path='/login' component={Login} /> */}
                     {/* <Route exact path='/dashboard' component={UserDashboard} /> */}
                     {/* <Route exact path='/search' component={Search} /> */}
                     {/* <Route exact path="/organization" component={OrgPublicPage} /> */}
