5c08a50c1d622719eab41dd9001523c896ee3d0e
Michael McKay <mamckay89@gmail.com>
2019-02-28 14:01:40 -0700

    rebase and push

--- src/components/search.js
+++ src/components/search.js
@@ -119,7 +119,7 @@ export default function Search(props) {
         fetchUserLocation();
         getEvent();
         getOrg();
-    }, [location, orgs]);
+    }, [orgs]);
 
     if (location === null) {
         return <div />;
