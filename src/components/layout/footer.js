9cb13008ed01e6f6ae6c616b0fb04fb5a7f7671c
carpenter-js <carpenterbarrett@gmail.com>
2019-02-28 12:42:25 -0700

    console err fixed

--- src/components/layout/footer.js
+++ src/components/layout/footer.js
@@ -2,21 +2,16 @@ import React from "react";
 
 const Footer = () => {
     return (
-        <section class='footer-container'>
+        <section className="footer-container">
             <footer>
                 <h1>About Helpful</h1>
-                <section class='about'>
-                    Helpful brings community organizations and volunteers
-                    together. If you want to make a difference in your
-                    community, you can find groups and events that would love to
-                    get your help. If you are an organization in need of
-                    volunteers, you can post your organization's profile and
-                    publicize your events here.
+                <section className="about">
+                    Helpful brings community organizations and volunteers together. If you want to make a difference in your community, you can find groups and events that would love to get your help. If you are an organization in need of volunteers, you can post your organization's profile and publicize your events here.   
                 </section>
             </footer>
-
-            <section class='sub-footer'>
-                <span>&copy; 2019 The Brogrammers</span>
+            
+            <section className="sub-footer">
+                <span>brogrammers&copy2019. Photos by rawpixel on Unsplash</span> 
             </section>
         </section>
     );
