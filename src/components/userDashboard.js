252b2cbd7f5e2ac3033f6310a2b8d3bcfb0e272c
Bryan Johnson <businessspamfolder@gmail.com>
2019-04-16 17:02:29 -0700

    active component user dashboard

--- src/components/userDashboard.js
+++ src/components/userDashboard.js
@@ -5,37 +5,91 @@ import FollowedOrgs from "./followedOrganizations";
 
 export default function UserDashboard() {
     const [view, setView] = useState(<EventList />);
-    // const [isActive, setIsActive] = useState(null)
+    const [isActive, setIsActive] = useState("EventList");
 
-    const handleClick = param => {
-        setView(param);
+    const handleActive = (renderedComponent, viewState) => {
+        setIsActive(renderedComponent);
+        setView(viewState);
     };
 
     return (
         <div className='container'>
             <div className='tabs is-centered'>
                 <ul>
-                    <li>
-                        <a
-                            href='#myevents'
-                            onClick={() => handleClick(<EventList />)}>
-                            My Events
-                        </a>
-                    </li>
-                    <li>
-                        <a
-                            href='#followed'
-                            onClick={() => handleClick(<FollowedOrgs />)}>
-                            Followed Orgs
-                        </a>
-                    </li>
-                    <li>
-                        <a
-                            href='#created'
-                            onClick={() => handleClick(<CreatedOrgs />)}>
-                            Created Orgs
-                        </a>
-                    </li>
+                    {isActive === "EventList" ? (
+                        <li className='is-active'>
+                            <a
+                                href='#myevents'
+                                onClick={() =>
+                                    handleActive("EventList", <EventList />)
+                                }>
+                                My Events
+                            </a>
+                        </li>
+                    ) : (
+                        <li>
+                            <a
+                                href='#myevents'
+                                onClick={() =>
+                                    handleActive("EventList", <EventList />)
+                                }>
+                                {" "}
+                                My Events
+                            </a>
+                        </li>
+                    )}
+
+                    {isActive === "FollowedOrgs" ? (
+                        <li className='is-active'>
+                            <a
+                                href='#followed'
+                                onClick={() =>
+                                    handleActive(
+                                        "FollowedOrgs",
+                                        <FollowedOrgs />
+                                    )
+                                }>
+                                Followed Orgs
+                            </a>
+                        </li>
+                    ) : (
+                        <li>
+                            <a
+                                href='#followed'
+                                onClick={() =>
+                                    handleActive(
+                                        "FollowedOrgs",
+                                        <FollowedOrgs />
+                                    )
+                                }>
+                                {" "}
+                                Followed Orgs
+                            </a>
+                        </li>
+                    )}
+
+                    {isActive === "CreatedOrgs" ? (
+                        <li className='is-active'>
+                            <a
+                                href='#created'
+                                onClick={() =>
+                                    handleActive("CreatedOrgs", <CreatedOrgs />)
+                                }>
+                                Created Orgs
+                            </a>
+                        </li>
+                    ) : (
+                        <li>
+                            <a
+                                href='#created'
+                                onClick={() =>
+                                    handleActive("CreatedOrgs", <CreatedOrgs />)
+                                }>
+                                {" "}
+                                Created Orgs
+                            </a>
+                        </li>
+                    )}
                 </ul>
             </div>
             {view}
